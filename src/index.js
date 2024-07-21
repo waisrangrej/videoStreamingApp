2e9204c50b8476e14a3a5685e3af1d13e2068bfa
waisrangrej <awaisrangrej9876@gmail.com>
2024-07-22 01:16:06 +0530

    added:connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express'
+// const app=express()
+
+// (async ()=>{
+//     try {
+//      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+// app.on("error",(error)=>{
+//     console.log("errr",error);
+//     throw error
+// })
+
+// app.listen(process.env.PORT,()=>{
+//     console.log(`server is listeing on ${process.env.PORT}`);
+// })
+//     } catch (error) {
+//         console.log("Error:",error);
+//         throw error
+//     }
+// })()
